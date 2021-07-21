fb00bb97b45ae6854ba04443d936bf0211df12ce
Orlando J Betancourth <orlando.betancourth@gmail.com>
2021-07-20 19:52:27 -0600

    Weather API and Sqlite DB

--- weatherAnalizer/climaModeloDatos.py
+++ weatherAnalizer/climaModeloDatos.py
@@ -4,24 +4,26 @@ class ClimaDatos:
   def __init__(self):
     self.conn = sqlite3.connect("weather.db")
     cursor = self.conn.cursor()
-    cursor.execute('''CREATE TABLE IF NOT EXISTS clima(
-      codigo INTEGER PRIMARY KEY AUTOINCREMET,
-      ciudad TEXT NOT NULL,
-      pais TEXT NOT NULL,
-      idciudad TEXT NOT NUll,
-      temperatura REAL NOT NULL,
-      humedad REAL NOT NULL,
-      presion REAL NOT NULL,
-      tempmax REAL NOT NULL,
-      tempmin REAL NOT NULL,
-      temppercibida REAL NOT NULL
-    );''')
-    self.con.commit()
+    cursor.execute("""CREATE TABLE IF NOT EXISTS clima (
+                      codigo INTEGER PRIMARY KEY AUTOINCREMENT,
+                      ciudad TEXT NOT NULL,
+                      pais TEXT NOT NULL,
+                      idciudad TEXT NOT NUll,
+                      temperatura REAL NOT NULL,
+                      humedad REAL NOT NULL,
+                      presion REAL NOT NULL,
+                      tempmax REAL NOT NULL,
+                      tempmin REAL NOT NULL,
+                      temppercibida REAL NOT NULL
+                    ); """
+    )
+    self.conn.commit()
   def agregarClima(self, ciudad, pais, idciudad, temperatura, humedad, presion, tempmax, tempmin, temppercibida):
     cursor = self.conn.cursor()
     cursor.execute(
         '''INSERT INTO clima (ciudad, pais, idciudad, temperatura, humedad,
         presion, tempmax, tempmin, temppercibida) VALUES (
         ?, ?, ?, ?, ?, ?, ?, ?, ?
-        ); ''', ciudad, pais, idciudad, temperatura, humedad, presion, tempmax, tempmin, temppercibida)
+        ); ''', (ciudad, pais, idciudad, temperatura, humedad, presion, tempmax, tempmin, temppercibida))
     self.conn.commit()
+    cursor.close()
